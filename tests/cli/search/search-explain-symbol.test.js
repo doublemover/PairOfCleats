03230bd2a0bc2cdc91229bcc971b6d2153e39012
doublemover <153689082+doublemover@users.noreply.github.com>
2026-02-12 00:17:17 -0500

    Isolate CLI search tests to temp repo roots

--- tests/cli/search/search-explain-symbol.test.js
+++ tests/cli/search/search-explain-symbol.test.js
@@ -4,13 +4,13 @@ import path from 'node:path';
 import { spawnSync } from 'node:child_process';
 import { getCombinedOutput } from '../../helpers/stdio.js';
 import { applyTestEnv } from '../../helpers/test-env.js';
+import { makeTempDir } from '../../helpers/temp.js';
 
 const root = process.cwd();
-const tempRoot = path.join(root, '.testCache', 'explain-symbol');
+const tempRoot = await makeTempDir('pairofcleats-explain-symbol-');
 const repoRoot = path.join(tempRoot, 'repo');
 const cacheRoot = path.join(tempRoot, 'cache');
 
-await fsPromises.rm(tempRoot, { recursive: true, force: true });
 await fsPromises.mkdir(repoRoot, { recursive: true });
 await fsPromises.mkdir(cacheRoot, { recursive: true });
 
